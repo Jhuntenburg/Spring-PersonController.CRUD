55d9ddc8c58b2228a05020df837225ceca242679
Joe <Jhuntenburg@gmail.com>
2023-08-16 14:56:06 -0400

    checkpoint

---
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -0,0 +1,26 @@
+package io.zipcoder.crudapp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+    String firstName;
+    String lastName;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+   private Long id;
+
+    public Person() {
+
+    }
+   Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+}
